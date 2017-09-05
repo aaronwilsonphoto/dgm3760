41f86e7da095996d3d09de5d2086575c23f09ebf
Aaron Wilson <aaronwilsonphoto@gmail.com>
2017-09-05 17:01:31 -0600

    01-sendMail Assignment

---
+++ 01-sendMail/recieve.php
@@ -0,0 +1,12 @@
+<?php
+$soda = $_GET['soda'];
+$zoom = $_GET['zoom'];
+
+echo $soda;
+
+
+
+
+?>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d319.94654604727776!2d-111.71102987903296!3d40.27882291371536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d9ae228c73ac3%3A0x1245943be9e2aeb9!2sComputer+Science+and+Engineering%2C+800+W+University+Pkwy%2C+Orem%2C+UT+84058!5e0!3m2!1sen!2sus!4v1504064493589" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
